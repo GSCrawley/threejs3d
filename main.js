6be35728713dc22fa3f5b8fdbaeadb97dc7e574f
Gideon Crawley <gscrawley@gmail.com>
2021-06-19 17:51:55 -0700

    moon is textured

--- main.js
+++ main.js
@@ -78,7 +78,22 @@ const gid = new THREE.Mesh(
   new THREE.MeshBasicMaterial( { map: gidTexture } )
 );
 
-scene.add(gid)
+// scene.add(gid)
+
+// Moon
+
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3,32,32),
+  new THREE.MeshStandardMaterial( {
+    map: moonTexture,
+    normalmap: normalTexture
+  })
+);
+
+scene.add(moon)
 
 // render method called to see Animation
 // set up game loop to render repeatedly
