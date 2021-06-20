fcf7def2db7676531e608838ed7517c27c5ab42a
Gideon Crawley <gscrawley@gmail.com>
2021-06-19 17:16:00 -0700

    we have controls

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import './style.css'
 
 import * as THREE from 'three';
-import { LessStencilFunc } from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // 1. Scene
 // 2. camera
@@ -33,10 +34,24 @@ renderer.render( scene, camera );
 // Mesh: geometry + material
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
-const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true } );
+const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } );
 const torus = new THREE.Mesh( geometry, material );
 
 scene.add(torus)
+
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(5,5,5)
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+
+scene.add(pointLight, ambientLight)
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200,50);
+scene.add(lightHelper, gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
 
 // render method called to see Animation
 // set up game loop to render repeatedly
@@ -48,6 +63,8 @@ function animate() {
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
+  controls.update();
+  
   renderer.render( scene, camera );
 
 }
