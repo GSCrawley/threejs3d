25faf028fbee8f14613297a751131e5074c708ce
Gideon Crawley <gscrawley@gmail.com>
2021-06-19 17:25:19 -0700

    added stars in random pattern

--- main.js
+++ main.js
@@ -52,6 +52,19 @@ scene.add(lightHelper, gridHelper)
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial( {color: 0xffffff });
+  const star = new THREE.Mesh( geometry, material );
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ) );
+
+  star.position.set(x,y,z);
+  scene.add(star)
+
+}
+
+Array(500).fill().forEach(addStar)
 
 // render method called to see Animation
 // set up game loop to render repeatedly
@@ -64,7 +77,7 @@ function animate() {
   torus.rotation.z += 0.01;
 
   controls.update();
-  
+
   renderer.render( scene, camera );
 
 }
